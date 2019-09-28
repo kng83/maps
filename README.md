e3f5ebdc52e61b5aecda1460691c90ad0113bba6
kng83 <pawel.kng@gmail.com>
2019-09-28 21:22:32 +0200

    end

--- README.md
+++
@@ -1,9 +0,0 @@
-//npm install -g parcel-bundler
-//parcel index.html
-//57 w kursie jest pokazane jak dodac google.maps i projekt w google
-//ctrl+shift+p i mamy menu w vscode i dajemy fold level 2 wtedy widzimy tylko nazwy np klas
-//tsc --init (initializacja tsc)
-//fajny concurently inaczej niz ja kiedys pisalem
-//npm install @types/node
-//composition vs inheritance 
-//book design Patterns Elements of reusable Object-Oriented Software Erich Gamma Richard Helm
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 
-import {MatchResult} from './MatchResult';
 import {MatchReader} from './MatchReader';
 import {CsvFileReader} from './CsvFileReader';
+import {Summary} from './Summary'
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import {HtmlReport} from './reportTargets/HtmlReport';
 // import {MatchReader} from './inheritance/MatchReader';
 
 
 
 const csvFileReader = new CsvFileReader('./football.csv'); 
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+const matchReader = MatchReader.fromCsv('./football.csv');
 matchReader.load();
 
 
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-} 
+// const summary1 = new Summary(new WinsAnalysis('Man United'),
+//        new HtmlReport());
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
+summary.buildAndPrintReport(matchReader.matches);
+    
