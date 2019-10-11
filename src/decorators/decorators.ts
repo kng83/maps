54c8f04e9d8d2f47ef8cd32d7b5616315eb3af8e
kng83 <pawel.kng@gmail.com>
2019-10-11 13:00:24 +0200

    end

--- src/decorators/decorators.ts
+++ src/decorators/decorators.ts
@@ -1,17 +1,28 @@
 
-
+@classDecorator
 class Boat {
-    @testDecorator
+    // @propDecorator
     color: string = 'red';
 
+    @propDecorator
     get formattedColor(): string {
         return `This bout color is ${this.color}`;
     }
 
-    @logError
+    @logError2('This is a new error')
     pilot(): void {
         throw new Error();
     }
+
+    howFast(@parameterDecorator speed: string, @parameterDecorator generateWake: string) {
+        if (speed === 'fast') {
+            console.log('swish')
+        } else {
+            console.log('ughhh')
+        }
+    }
+
+
 }
 
 /* Target to bedzie nasz prototyp w tym wypadku bedzie to proptyp  Boat czyli wszystkie funkcje i gettery 
@@ -22,17 +33,50 @@ function testDecorator(target: any, key: string): void {
     console.log('key:', key);
 }
 
+
+// tutja dekorator z na stale wpisanym bledem
 function logError(target: any, key: string, desc: PropertyDescriptor): void {
     const method = desc.value;
-    desc.value = function (){
-        try{
+    desc.value = function () {
+        try {
             method();
 
-        }catch(e){
+        } catch (e) {
             console.log('Boat was sinking !!!! error from decorator');
         }
     }
 }
+
+// Wersja dekoratora z parametrem (zwracamy funkcje dekoratora z funkcji)
+//ten patent to dekorator factory
+function logError2(errorMessage: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
+        desc.value = function () {
+            try {
+                method();
+
+            } catch (e) {
+                console.log(errorMessage);
+            }
+        }
+    }
+}
+
+function propDecorator(target: any, key: string) {
+    console.log(target, key, 'prop decorator');
+}
+
+// index w parametrach jest numerem argumentu dla parametrow
+function parameterDecorator(target: any, key: string, index: number) {
+    console.log('--parametr decorator--------', key, index, target);
+}
+
+
+function classDecorator(constructor:typeof Boat){
+    console.log("**************", constructor,"*************");
+} 
+
 
 //jak by nie uzywac dekoratorów to bedzie dzialac identycznie
 //to dziala jeszcze przed zrobienie instancji classy
