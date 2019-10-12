d22ab07bec910a1d5f269f678b1c78ed34478503
kng83 <pawel.kng@gmail.com>
2019-10-12 16:43:12 +0200

    end

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component{
-    render(){
-        return <div>Hi there!!!!</div>
+
+interface AppProps {
+    todos: Todo[];
+    fetchTodos(): any;
+}
+class _App extends React.Component<AppProps>{
+    onButtonClick = ():void => {
+        this.props.fetchTodos();
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onButtonClick}></button>
+            </div>)
     }
 }
+
+
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+    return { todos }
+}
+
+export const App = connect(
+    mapStateToProps,
+    { fetchTodos }
+)(_App);
