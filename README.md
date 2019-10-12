8cbbd2f26d3b760a4a2802ff83c51da8ae3d90ed
kng83 <pawel.kng@gmail.com>
2019-10-12 16:03:20 +0200

    s

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 //parcel index.html
 //57 w kursie jest pokazane jak dodac google.maps i projekt w google
 //ctrl+shift+p i mamy menu w vscode i dajemy fold level 2 wtedy widzimy tylko nazwy np klas
-npx create-react-app rrts --typescript
+npx create-react-app rrts --typescript // to jest do reakta
+npm install redux react-redux axios redux-thunk // to do redux
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {App} from './components/App';
+import { reducers} from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    ,
+    document.querySelector('#root')
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// to nam daje structure props ktore dajemy do komponentu
+// interface AppProps {
+//     color?: string;
+// }
+
+// interface AppState{
+//     counter:number;
+// }
+
+// class App extends React.Component<AppProps> {
+
+//     // to albo genderic w konstruktorze
+//     state = {
+//         counter: 0
+//     };
+
+//     // constructor(public props: AppProps) {
+//     //     super(props);
+//     //     this.state = { counter: 0 };
+//     // }
+
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 })
+//     };
+
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Decrement</button>
+//                 {this.state.counter}
+//             </div>
+//         )
+//     }
+// }
+
+// example of funcional component
+// const App = (props: AppProps):JSX.Element =>{
+//     return <div>{props.color}</div>
+// }
+
