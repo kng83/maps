9fda2d998300e1e896457fc2d42f109e6802b220
kng83 <pawel.kng@gmail.com>
2019-11-03 03:33:02 +0100

    some

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 //import './patterns/behavioral_patterns/state_pattern';
 //import './patterns/behavioral_patterns/template_pattern';
-import './patterns/behavioral_patterns/observer_pattern_3';
+//import './patterns/behavioral_patterns/observer_pattern_3';
+import './patterns/behavioral_patterns/visitor_pattern';
