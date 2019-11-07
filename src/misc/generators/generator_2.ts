41c7a4c3dbc37da090c2cb2014ad581c4c124a54
kng83 <pawel.kng@gmail.com>
2019-11-07 17:42:37 +0100

    en

--- src/misc/generators/generator_2.ts
+++ src/misc/generators/generator_2.ts
@@ -14,11 +14,14 @@ namespace Generator_2{
         console.log(color); // czerwony ,czarny
     }
 
+
+/*----------------------------------*/console.log('--------------------------------');
     // teraz test z klasa
 
     class Colors{
         red = 'czerwony';
         black = 'czarny';
+        pink  = 'rozowy';
 
         *[Symbol.iterator](){
             yield this.red;
@@ -29,5 +32,35 @@ namespace Generator_2{
     let colorsPallet = new Colors();
     for(let c of colors){
         console.log(c);
+    }
+
+    /*----------------------------------*/console.log('--------------------------------');
+    class SuperColor extends Colors{
+    //    red='czerwony';
+    //  black = 'czarny';
+        blue = 'niebieski';
+
+        makeMe(){
+            'some text';
+        }
+        ape = () =>{
+            return 'malpa'
+        }
+
+        *[Symbol.iterator](){
+            let properties=  Object.getOwnPropertyNames(this);
+                for(let prop of properties){
+                    if(typeof (this as any)[prop] !== 'function'){
+                        yield (this as any)[prop];
+                    }
+
+            }
+        }
+
+    }
+
+    let superColor = new SuperColor();
+    for(let sColor of superColor){
+        console.log(sColor); // brane sa rowniez pola dziedziczone (funkcje i gettery i settery nie sa brane po uwage)
     }
 }
